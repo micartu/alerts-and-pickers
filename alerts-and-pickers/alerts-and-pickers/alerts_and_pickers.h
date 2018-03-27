73a78b6725cf176984dcad064bdef70726e8a0d6
Michael Artuerhof <michael.artuerhof@gmail.com>
2018-03-27 12:20:21 +0300

    - created a framework for working with other projects as a submodule

---
+++ alerts-and-pickers/alerts-and-pickers/alerts_and_pickers.h
@@ -0,0 +1,19 @@
+//
+//  alerts_and_pickers.h
+//  alerts-and-pickers
+//
+//  Created by Michael Artuerhof on 27.03.18.
+//  Copyright © 2018 BearMonti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for alerts_and_pickers.
+FOUNDATION_EXPORT double alerts_and_pickersVersionNumber;
+
+//! Project version string for alerts_and_pickers.
+FOUNDATION_EXPORT const unsigned char alerts_and_pickersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <alerts_and_pickers/PublicHeader.h>
+
+
